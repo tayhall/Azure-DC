b4be16897e2f8af66365d22ea018dea1f0b3981e
tayhall <4ndrewhall@gmail.com>
2020-03-20 13:57:29 +0000

    vars updating

--- deploy.ps1
+++ deploy.ps1
@@ -5,25 +5,25 @@ $AdminPassword = "https://coronavirus.vault.azure.net/secrets/ITSecret/4beac336d
 $DomainName = "SCUKDOM.local"
 $DnsPrefix = "SCUKDOM"
 $VmSize = "Standard_DS1_V2"
-$_artifactsLocation
-$_artifactsLocationSasToken
+$_artifactsLocation = ""
+$_artifactsLocationSasToken ""
 $VirtualMachineName = "MYDC1"
 $VirtualNetworkName = "adLAN"
 $VirtualNetworkAddressRange = "172.16.1.0/24"
-$LoadBalancerFEIPName
-$BackendAddressPoolName
+$LoadBalancerFEIPName = "LBFE"
+$BackendAddressPoolName = "LBBE"
 $ResourceGroupName = $DnsPrefix + "rg"
-$InboundNatRulesName "adRDP"
-$NetworkInterfaceName "adNic"
+$InboundNatRulesName = "adRDP"
+$NetworkInterfaceName = "adNic"
 $PrivateIPAddress = "172.16.100.100"
 $SubnetName = "adSubnet"
 $SubnetRange = "172.16.100.0/24"
 $PublicIPAddressName = "adPublicIP"
-$AvailabilitySetName "adAvailabiltySet"
+$AvailabilitySetName = "adAvailabiltySet"
 $LoadBalancerName  = "adLoadBalancer"
 
 
 
 # Deploy the new domain
 New-AzResourceGroup -Name $ResourceGroupName -Location $Location 
-New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile <path-to-template> 
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateURI "https://github.com/tayhall/Azure-DC/blob/master/azuredeploy.json"
