5a8dcc0c14ecd08c290d9c3ef3f9c31baff87765
tayhall <4ndrewhall@gmail.com>
2020-03-20 14:18:28 +0000

    Public IP code

--- deploy.ps1
+++ deploy.ps1
@@ -2,11 +2,11 @@
 $Location = "East US" 
 $AdminUsername = Andrew
 $AdminPassword = "https://coronavirus.vault.azure.net/secrets/ITSecret/4beac336dbde423498234823cf2dde86"
-$DomainName = "SCUKDOM.local"
+$DomainName = "SCUKDOM"
 $DnsPrefix = "SCUKDOM"
 $VmSize = "Standard_DS1_V2"
 $_artifactsLocation = ""
-$_artifactsLocationSasToken ""
+$_artifactsLocationSasToken = ""
 $VirtualMachineName = "MYDC1"
 $VirtualNetworkName = "adLAN"
 $VirtualNetworkAddressRange = "172.16.1.0/24"
@@ -16,7 +16,7 @@ $ResourceGroupName = $DnsPrefix + "rg"
 $InboundNatRulesName = "adRDP"
 $NetworkInterfaceName = "adNic"
 $PrivateIPAddress = "172.16.100.100"
-$SubnetName = "adSubnet"
+$SubnetName = "adSubnet" 
 $SubnetRange = "172.16.100.0/24"
 $PublicIPAddressName = "adPublicIP"
 $AvailabilitySetName = "adAvailabiltySet"
@@ -24,6 +24,14 @@ $LoadBalancerName  = "adLoadBalancer"
 
 
 
-# Deploy the new domain
+# Deploy the new resource group
 New-AzResourceGroup -Name $ResourceGroupName -Location $Location 
-New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateURI "https://github.com/tayhall/Azure-DC/blob/master/azuredeploy.json"
+
+# Assign public IP
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateParameterUri -TemplateURI "https://github.com/tayhall/Azure-DC/blob/master/azuredeploy.json";
+-DomainNameLabel $dnsPrefix 
+
+
+# Deploy the DC
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateURI "https://github.com/tayhall/Azure-DC/blob/master/azuredeploy.json";
+-adminUsername $AdminUsername -adminPassword $AdminPassword
