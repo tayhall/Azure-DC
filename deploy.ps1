25d132340054cd79373b3ccdade8ccf5cce55438
tayhall <4ndrewhall@gmail.com>
2020-03-25 15:34:24 +0000

    Key Vault UPdates

--- deploy.ps1
+++ deploy.ps1
@@ -1,19 +1,45 @@
+<#
+Default Parameters
+
+Name                          Type                       Value
+============================  =========================  ==========
+adminUsername                 String                     Andrew
+adminPassword                 SecureString
+domainName                    String                     corp.SCUKDOM.local
+dnsPrefix                     String                     andybuswan
+vmSize                        String                     Standard_D2s_v3
+_artifactsLocation            String                     https://raw.githubusercontent.com/tayhall/Azure-DC/master/azuredeploy.json
+_artifactsLocationSasToken    SecureString
+location                      String                     eastus
+virtualMachineName            String                     adVM
+virtualNetworkName            String                     adVNET
+virtualNetworkAddressRange    String                     10.0.0.0/16
+loadBalancerFrontEndIPName    String                     LBFE
+backendAddressPoolName        String                     LBBE
+inboundNatRulesName           String                     adRDP
+networkInterfaceName          String                     adNic
+privateIPAddress              String                     10.0.0.4
+subnetName                    String                     adSubnet
+subnetRange                   String                     10.0.0.0/24
+publicIPAddressName           String                     adPublicIP
+availabilitySetName           String                     adAvailabiltySet
+loadBalancerName              String                     adLoadBalancer
+
+#>
 # Inital Variables
 $Location = "East US" 
 $AdminUsername = "Andrew"
 $AdminPassword = "https://coronavirus.vault.azure.net/secrets/ITSecret/4beac336dbde423498234823cf2dde86"
-$DomainName = "SCUKDOM"
+$domainName = "corp.SCUKDOM.local"
 $domainNameLabel ="SCUKDOM"
-$DnsPrefix = "SCUKDOM"
+$dnsPrefix = "andybuswan6"
 $VmSize = "Standard_DS1_V2"
-$_artifactsLocation = ""
-$_artifactsLocationSasToken = ""
 $VirtualMachineName = "MYDC1"
 $VirtualNetworkName = "adLAN"
 $VirtualNetworkAddressRange = "172.16.1.0/24"
 $LoadBalancerFEIPName = "LBFE"
 $BackendAddressPoolName = "LBBE"
-$ResourceGroupName = "IAASrg"
+$ResourceGroupName = "IAASrg6"
 $InboundNatRulesName = "adRDP"
 $NetworkInterfaceName = "adNic"
 $PrivateIPAddress = "172.16.100.100"
@@ -23,15 +49,34 @@ $PublicIPAddressName = "adPublicIP"
 $AvailabilitySetName = "adAvailabiltySet"
 $LoadBalancerName  = "adLoadBalancer"
 
-
+$ResourceGroupName = Read-Host -Prompt "Enter a project name that is used for generating resource names"
 
 # Deploy the new resource group
-New-AzResourceGroup -Name $ResourceGroupName -Location $Location 
+#New-AzResourceGroup -Name $ResourceGroupName -Location $Location 
+
+# Create a keyVault
+
+$upn = Read-Host -Prompt "Enter your user principal name (email address) used to sign in to Azure"
+$secretValue = Read-Host -Prompt "Enter the virtual machine administrator password" -AsSecureString
+
+$resourceGroupName = "${projectName}rg"
+$keyVaultName = $projectName
+$adUserId = (Get-AzADUser -UserPrincipalName $upn).Id
+$templateUri = "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/tutorials-use-key-vault/CreateKeyVault.json"
+
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -keyVaultName $keyVaultName -adUserId $adUserId -secretValue $secretValue
 
 # Assign public IP Range
-New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateParameterUri "https://raw.githubusercontent.com/tayhall/Azure-DC/5a8dcc0c14ecd08c290d9c3ef3f9c31baff87765/publicip/parameters.json" -TemplateURI "https://raw.githubusercontent.com/tayhall/Azure-DC/5a8dcc0c14ecd08c290d9c3ef3f9c31baff87765/publicip/template.json"
+#New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateParameterUri "https://raw.githubusercontent.com/tayhall/Azure-DC/5a8dcc0c14ecd08c290d9c3ef3f9c31baff87765/publicip/parameters.json" -TemplateURI "https://raw.githubusercontent.com/tayhall/Azure-DC/5a8dcc0c14ecd08c290d9c3ef3f9c31baff87765/publicip/template.json"
 
-
+#$cred = Get-Credential
+#$Pass = "ILikeSecurePassword1!"
+#$SecurePass = ConvertTo-SecureString -String $Pass -AsPlainText -Force
 # Deploy the DC
-New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateURI "https://raw.githubusercontent.com/tayhall/Azure-DC/master/azuredeploy.json";
--adminUsername $AdminUsername -adminPassword $AdminPassword
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+-TemplateURI "https://raw.githubusercontent.com/tayhall/Azure-DC/master/azuredeploy.json" `
+-adminUsername $AdminUsername -adminPassword $SecurePass -domainName $domainName -dnsPrefix $dnsPrefix
+
+
+#-VmSize $VmSize
