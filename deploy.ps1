9969f10977bc8f3efba8772b53826207520bc29c
tayhall <4ndrewhall@gmail.com>
2020-04-07 21:51:49 +0100

    Updated and fixed adUserID

--- deploy.ps1
+++ deploy.ps1
@@ -31,7 +31,7 @@ $Location = "West US 2"
 $domainName = "corp.UKTSTDOM.local"
 $domainNameLabel ="UKTSTDOM"
 $dnsPrefix = "myDNSPre"
-$resourceGroupName = "azDomrg"
+#$resourceGroupName = "azDomrg"
 $VmSize = "Standard_DS1_V2"
 
 $projectName = Read-Host -Prompt "Enter a project name that is used for generating resource names"
@@ -39,11 +39,11 @@ $projectName = Read-Host -Prompt "Enter a project name that is used for generati
 # Create a keyVault
 
 # $upn = Read-Host -Prompt "Enter your user principal name (email address) used to sign in to Azure"
-$upn = "AndyDevApril_outlook.com#EXT#@AndyDevTrainoutlook.onmicrosoft.com"
+$upn = "Andrew Hall"
 $secretValue = Read-Host -Prompt "Enter the virtual machine administrator password" -AsSecureString
-$resourceGroupName = "${projectName}rg"
-$keyVaultName = $projectName
-$adUserId = (Get-AzADUser -UserPrincipalName $upn).Id
+$resourceGroupName = $projectName + "rg"
+$keyVaultName = 'mySecretLocker99'
+$adUserId = (Get-AzADUser -DisplayName $upn).Id
 $templateUri = "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/tutorials-use-key-vault/CreateKeyVault.json"
 
 New-AzResourceGroup -Name $resourceGroupName -Location $location
